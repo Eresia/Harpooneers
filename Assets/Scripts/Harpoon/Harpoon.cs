9fe442858a7af12c831684ca6ba5c265eee0189f
Bastien LEPESANT <bastien@lepesant.me>
2018-03-06 10:25:57 +0100

    Add Good folders

---
+++ Assets/Scripts/Harpoon/Harpoon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harpoon : MonoBehaviour {
+
+	[SerializeField]
+	private HarpoonLauncher parent;
+
+	[SerializeField]
+	private float maxDistance;
+
+	private float actualDistance;
+}
---
+++ Assets/Scripts/Harpoon/HarpoonLauncher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarpoonLauncher : MonoBehaviour {
+
+	[SerializeField]
+	private float castTime;
+
+	[SerializeField]
+	private float castDistance;
+
+	[SerializeField]
+	private float launchDistanceMax;
+	
+	[SerializeField]
+	private float cooldownTime;
+}
