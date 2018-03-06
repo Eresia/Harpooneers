de9345f0b8ed4318ca4a3d41fa2ee09399b6f3ff
Bastien LEPESANT <bastien@lepesant.me>
2018-03-06 16:43:50 +0100

    Add Pull/Release on Harpoon & Direction correction

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public bool debug;
+	public float actualPlayer;
+
+	public static GameManager instance {get; private set;}
+
+	void Awake()
+	{
+		if (instance == null){
+			instance = this;
+		}
+		else if (instance != this){
+			Destroy(gameObject);   
+		}
+	}
+}
