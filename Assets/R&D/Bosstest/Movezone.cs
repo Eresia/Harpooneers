813ee2d4246937d7b2a9e7aa63eb33b98cbe908b
Unknown <hellcatvi.59@hotmail.fr>
2018-03-08 17:54:50 +0100

    R&D start for the Boss Pattern, R&D folder is for this

---
+++ Assets/R&D/Bosstest/Movezone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movezone : MonoBehaviour
+{
+    private BoxCollider _movezone;
+    public Vector3 Offset;
+
+    private void Awake()
+    {
+        _movezone = GetComponent<BoxCollider>();
+        _movezone.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            other.gameObject.transform.position += Offset;
+    }
+}
