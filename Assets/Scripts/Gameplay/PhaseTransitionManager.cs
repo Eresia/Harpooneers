ebcd377617e110f40a791e686645897bcee5c3e6
Own3dbysquirrel <own3dbysquirrel@gmail.com>
2018-03-09 14:59:31 +0100

    Phase Transition Manager

---
+++ Assets/Scripts/Gameplay/PhaseTransitionManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PhaseTransition
+{
+    public Vector3 camPosition;
+    public float transitionTime;
+}
+
+public class PhaseTransitionManager : MonoBehaviour {
+
+    public Transform cameraTransform;
+    public int currentPhase = -1;
+    [Space(20)]
+    public PhaseTransition[] phasesCameras;
+    public bool gameOver;
+    
+
+
+    // For Debug / Manual
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P) && !gameOver)
+        {           
+            NextPhase();
+        }
+    }
+
+    public void NextPhase()
+    {
+        currentPhase++;
+
+        if(currentPhase < phasesCameras.Length)
+        {
+            StartCoroutine(TransitionToNextPhase(cameraTransform.position, phasesCameras[currentPhase].camPosition, phasesCameras[currentPhase].transitionTime));
+        }       
+        else
+        {
+            gameOver = true;
+            Debug.Log("You won !");
+        }
+
+    }
+
+    IEnumerator TransitionToNextPhase(Vector3 startPos, Vector3 endPos, float time)
+    {
+        Debug.Log("yolo");
+        float elapsedTime = 0;
+
+        while (elapsedTime < time)
+        {
+            cameraTransform.position = Vector3.Lerp(startPos, endPos, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
+
+
