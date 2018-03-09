5ee63f2be16088364e74c65672133c37d1ab02f4
Devillers Lucas <devillerslucas@gmail.com>
2018-03-09 17:00:16 +0100

    Frag V0.1 ok

---
+++ Assets/R&D/Fragmentation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fragmentation : MonoBehaviour
+{	public GameObject Output;
+
+	public void OnExplode ()
+	{
+		Debug.Log("BOOM");
+		Output.SetActive(true);
+		gameObject.SetActive(false);
+	}
+}
