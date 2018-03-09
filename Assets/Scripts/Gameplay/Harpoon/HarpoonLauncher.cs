388c6e1eb0ba726ab9a64723efb44cd34703733d
Devillers Lucas <devillerslucas@gmail.com>
2018-03-09 16:00:34 +0100

    R&D Dash Baleine

--- Assets/Scripts/Gameplay/Harpoon/HarpoonLauncher.cs
+++ Assets/Scripts/Gameplay/Harpoon/HarpoonLauncher.cs
@@ -100,7 +100,7 @@ public class HarpoonLauncher : MonoBehaviour {
 
 		if(harpoon == null){
 
-            Debug.Log(direction.sqrMagnitude);
+            //Debug.Log(direction.sqrMagnitude);
 
 			if(direction.sqrMagnitude > joystixError)
             {
