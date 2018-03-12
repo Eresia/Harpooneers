7a37c8232b9cfcb0cbce989e27e1defb6de5a156
Bastien LEPESANT <bastien@lepesant.me>
2018-03-12 17:20:48 +0100

    Add new waves + clean ground class

--- Assets/Scripts/NewPhysic/Waves/WaveImpact.cs
+++ Assets/Scripts/NewPhysic/Waves/WaveImpact.cs
@@ -2,25 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveImpact : Wave_a {
+// public class WaveImpact : Wave_a {
 
-	public WaveImpact(WaveOptions options) : base(options){}
+// 	public WaveImpact(WaveOptions options) : base(options){}
 
-	public override float CalculateTheta(Vector2 offset, Vector2Int lod, float wt, Vector2 direction, Vector2 posAbs){
-		float thetaX = (options.waveNumber * (offset.x / lod.x)) - (wt * direction.x);
-		float thetaY = (options.waveNumber * (offset.y / lod.y)) - (wt * direction.y);
+// 	public override float CalculateTheta(Vector2 offset, Vector2Int lod, float wt, Vector2 direction, Vector2 posAbs){
+// 		float thetaX = (options.waveNumber * (offset.x / lod.x)) - (wt * direction.x);
+// 		float thetaY = (options.waveNumber * (offset.y / lod.y)) - (wt * direction.y);
 
-		float timeChange = 1 / Mathf.Exp(wt * options.timeDigress);
+// 		float timeChange = 1 / Mathf.Exp(wt * options.timeDigress);
 
-		float xHeight = Mathf.Sin(thetaX) / Mathf.Exp(posAbs.x * options.distanceDigress) * timeChange;
-		float yHeight = Mathf.Sin(thetaY) / Mathf.Exp(posAbs.y * options.distanceDigress) * timeChange;
+// 		float xHeight = Mathf.Sin(thetaX) / Mathf.Exp(posAbs.x * options.distanceDigress) * timeChange;
+// 		float yHeight = Mathf.Sin(thetaY) / Mathf.Exp(posAbs.y * options.distanceDigress) * timeChange;
 		
-		float theta = (posAbs.x * xHeight + posAbs.y * yHeight) / (posAbs.x + posAbs.y);
+// 		float theta = (posAbs.x * xHeight + posAbs.y * yHeight) / (posAbs.x + posAbs.y);
 
-		return theta;
-	}
+// 		return theta;
+// 	}
 
-	protected override bool IsTooFar(Vector2 offset){
-		return ((offset.x > 20) || (offset.y > 20));
-	}
-}
+// 	protected override bool IsTooFar(Vector2 offset){
+// 		return ((offset.x > 20) || (offset.y > 20));
+// 	}
+// }
