4c787dd03840014e762141ddaa699f64709d6a79
Own3dbysquirrel <own3dbysquirrel@gmail.com>
2018-03-13 14:16:16 +0100

    added script for Whale FX

---
+++ Assets/Scripts/Gameplay/Boss/WhaleFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleFX : MonoBehaviour {
+
+    public ParticleSystem[] eyeBloodFX;
+    public ParticleSystem splashFX;
+
+    public void  PlayEyeBloodFX(int i)
+    {
+        eyeBloodFX[i].Play();
+    }
+    public void PlaySplashFX()
+    {
+        splashFX.Play();
+    }
+
+}
